fc478c8058ad15152d5a0c2f3c3cc7f3bb2ac308
keastin2814 <keastin2814@gmail.com>
2017-01-22 00:37:32 -0600

    Add files via upload

---
+++ Assets/Scripts/WaveGen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGen : MonoBehaviour {
+
+    // Use this for initialization
+
+    void Start()
+    {
+       
+    }
+
+
+
+        // Update is called once per frame
+        void Update () {
+        transform.Translate(1f, 0, 0);
+        if (transform.position.x >=6)
+            transform.localScale += new Vector3(0, -0.2f, 0);
+        else if (transform.localScale.y <1.1f)
+                transform.localScale += new Vector3(0, 0.2f,0);
+        if (transform.position.x >=10f)
+        {
+            transform.Translate(-26f,0,0);
+        }
+	}
+}
+
---
+++ Assets/Scripts/WaveGen2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGen2 : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-1f, 0, 0);
+        if (transform.position.x <= 39)
+            transform.localScale += new Vector3(0, -0.2f, 0);
+        else if (transform.localScale.y < .9f)
+            transform.localScale += new Vector3(0, 0.2f, 0);
+        if (transform.position.x <= 35f)
+        {
+            transform.Translate(14f, 0, 0);
+        }
+    }
+}
